e29ee2a803f0a0c3e10b303ac0e8d7157c5aafd7
Alberto Caparrós Gutiérrez <acapgut022@g.educaand.es>
2025-11-20 18:49:39 +0100

    comentarios añadidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,11 @@
+/**
+* Muestra por pantalla la frase "Hola Mundo"
+*
+*
+* @autor Alberto
+*/
+
+
 public class HolaMundo {
   public static void main(String[] args) {
 	System.out.println("Hola Mundo");
